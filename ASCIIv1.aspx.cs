207a05d1257fb54af2ae0957f91d6c2ae4ad1219
ccngtitan <ccngtitan@gmail.com>
2017-06-10 11:19:06 +0800

    Add files via upload

---
+++ ASCIIv1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class ASCIIv1 : System.Web.UI.Page
+    {
+        public string strHtml = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                lbl1.Text += i + " " + Convert.ToChar(i) + "<br/>";
+            }
+        }
+    }
+}
